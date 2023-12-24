0776dd4b746de3960b2598260b56abfd9f3037d3
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2023-12-24 17:53:37 +0200

    Update blocksroutes.ts

--- src/routes/blocksroutes.ts
+++ src/routes/blocksroutes.ts
@@ -16,7 +16,7 @@ blocksroute.get("/blocks/",(req,res)=>{
 blocksroute.post("/block/create",(req,res)=>{
    let lastblock:Block;
    let block:Block=<Block>{
-      trans:Trans[],
+      trans:[],
       prevhash:lastblock.hash
     }
    AppDataSource.manager.findOne(Block,{
