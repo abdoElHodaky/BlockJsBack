7d54239a729a0cf7c4ba14d0e1d5876d71437e6d
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2023-12-24 15:24:08 +0200

    Create Trans.ts

---
+++ src/entity/Trans.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, TableInheritance } from "typeorm"
+import { Block } from "./User";
+@Entity()
+export class Trans {
+    @PrimaryGeneratedColumn("increment")
+    id:number
+    @Column({default:""})
+    from: string;
+    @Column({default:""})
+    to:string;
+    @Column({default:0})
+    amount:number 
+
+    @ManyToOne(()=>Block,block=>block.trans) block:Block
+}
