aa5e0c58d018093df6caf322814d742bbf6330e4
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2023-12-24 15:18:29 +0200

    Create Block.ts

---
+++ src/entity/Block.ts
@@ -0,0 +1,16 @@
+import { Entity,Column,OneToMany,PrimaryGeneratedColumn} from "typeorm"
+import { Trans } from "./Trans";
+
+@Entity()
+export class Block {
+    @PrimaryGeneratedColumn("increment")
+    id: number;
+
+    @Column({type: "varchar", nullable: true})
+    hash: string;
+
+    @Column({type: "varchar"})
+    prevhash: string
+
+    @OneToMany(()=>,b=>author.articles) trans:Trans[];
+}
